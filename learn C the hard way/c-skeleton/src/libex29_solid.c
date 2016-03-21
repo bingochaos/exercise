0f9a30ca7a7b1fce84636b872d68172b3ae7987e
bingochaos <bingoc1992@163.com>
2016-03-21 20:54:46 +0800

    libex29_solid

--- learn C the hard way/c-skeleton/src/libex29_solid.c
+++ learn C the hard way/c-skeleton/src/libex29_solid.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "dbg.h"
+#include <assert.h>
 
-int print_a_message(const char *msg)
+int print_a_message_s(const char *msg)
 {
+	assert(msg != NULL);
+	
 	printf("A STRING: %s\n", msg);
 
 	return 0;
 }
 
-int uppercase(const char *msg, const int len)
+int uppercase_s(const char *msg, const int len)
 {
+	assert(msg != NULL);
+
 	int i = 0;
 
 	for(i = 0; i < len; i++)
@@ -23,8 +28,10 @@ int uppercase(const char *msg, const int len)
 	return 0;
 }
 
-int lowercase(const char *msg, const int len)
+int lowercase_s(const char *msg, const int len)
 {
+	assert(msg != NULL);
+
 	int i = 0;
 
 	for(i = 0; i < len; i++)
@@ -37,7 +44,7 @@ int lowercase(const char *msg, const int len)
 	return 0;
 }
 
-int fail_on_purpose(const char *msg)
+int fail_on_purpose_s(const char *msg)
 {
 	return 1;
 }
