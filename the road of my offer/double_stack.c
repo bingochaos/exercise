99000dab00d7e698658ac940a7c79fb9d7482f82
bingochaos <bingoc1992@163.com>
2016-03-25 17:33:25 +0800

    数组实现两个栈

--- the road of my offer/double_stack.c
+++ the road of my offer/double_stack.c
@@ -2,8 +2,8 @@
 
 struct double_stack
 {
-	int topindexA = 0;
-	int topindexB = MAX_LEN - 1;
+	int topindexA = -1;
+	int topindexB = MAX_LEN;
 	int a[MAX_LEN];
 };
 
@@ -18,7 +18,7 @@ void pushA(int a[], int i)
 
 int popA(int a[])
 {
-	if(topindexA == 0)
+	if(topindexA == -1)
 		printf("A is empty\n");
 
 	int tmp = a[topindexA];
@@ -38,7 +38,7 @@ void pushB(int a[], int i)
 
 int popB(int a[])
 {
-	if(topindexB == MAX_LEN - 1)
+	if(topindexB == MAX_LEN)
 		printf("B is empty\n");
 
 	int tmp = a[topindexB];
